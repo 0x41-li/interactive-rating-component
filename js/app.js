75966c55dbce6e130333fb42e437e63505fafea1
Ali Sbane <ali.sbane.dev@gmail.com>
2022-04-11 18:00:26 +0000

    the functionality of chosing a rating number added, plus submit button hide the rating comp

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
 (function (window, document) {
-  let ratingNumbers = document.querySelectorAll(".rating-comp__rate-numbers span");
+
+  let ratingComp = document.querySelector('.rating-comp');
+  let ratingNumbers = document.querySelectorAll(
+    ".rating-comp__rate-numbers span"
+  );
+  let submitBtn = document.querySelector(".rating-comp__submit");
 
   ratingNumbers.forEach((number) => {
     number.addEventListener("click", () => {
-      console.log(number);
+      ratingNumbers.forEach((n) => {
+        n.classList.remove("chosen");
+      });
+
+      number.classList.add("chosen");
     });
   });
+
+
+  submitBtn.addEventListener("click", () => {
+    ratingComp.classList.add("hide");
+  });
+
 })(window, document);
