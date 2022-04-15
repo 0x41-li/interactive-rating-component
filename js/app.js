9e58d1ccc1701fbfe62ec4be0f51b7ee88c77625
Ali Sbane <ali.sbane.dev@gmail.com>
2022-04-15 12:09:11 +0000

    The design and functionality Completed

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
 (function (window, document) {
+  let ratingComp = document.querySelector(".rating-comp");
+  let thankYouComp = document.querySelector(".thank-you-comp");
 
-  let ratingComp = document.querySelector('.rating-comp');
   let ratingNumbers = document.querySelectorAll(
     ".rating-comp__rate-numbers span"
   );
+
+  let ratingSelectedNumber = document.querySelector(".rating-selected-number");
+
   let submitBtn = document.querySelector(".rating-comp__submit");
 
   ratingNumbers.forEach((number) => {
@@ -16,9 +20,13 @@
     });
   });
 
-
   submitBtn.addEventListener("click", () => {
-    ratingComp.classList.add("hide");
-  });
 
+    let chosenRating = document.querySelector(".chosen");
+
+
+    ratingSelectedNumber.textContent = chosenRating.textContent;
+    ratingComp.classList.add("hide");
+    thankYouComp.classList.remove("hide");
+  });
 })(window, document);
